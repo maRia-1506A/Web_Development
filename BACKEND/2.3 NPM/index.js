e3c4d2f97ff8243ec3713df7e21c334e97625201
maRia-1506A <zannatulfarzana363@gmail.com>
2025-11-07 22:06:20 +0600

    modified

--- BACKEND/2.3 NPM/index.js
+++ BACKEND/2.3 NPM/index.js
@@ -1,12 +1,24 @@
-// var generateName = require('sillyname');
+// // var generateName = require('sillyname');
 
-// import generateName from "sillyname";
-// var sillyName = generateName();
-// console.log("My name is "+sillyName);
+// // import generateName from "sillyname";
+// // var sillyName = generateName();
+// // console.log("My name is "+sillyName);
 
 
-import { randomSuperhero } from "superheroes";
+// import { randomSuperhero } from "superheroes";
 
-const name = randomSuperhero();
-console.log(`My name is ${name}`);
+// const name = randomSuperhero();
+// console.log(`My name is ${name}`);
 
+// var generateName = require("sillyname");
+import generateName from "sillyName";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
