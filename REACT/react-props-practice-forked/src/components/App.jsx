23c10f2ebadb53e5cfc7cac9999a99d8ae23b5ae
maRia-1506A <zannatulfarzana363@gmail.com>
2025-11-14 20:12:39 +0600

    props

---
+++ REACT/react-props-practice-forked/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+
+      {/* card but as the contacts is array, using the index numb */}
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        mobile={contacts[0].phone}
+        email={contacts[0].email}
+      />
+
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        mobile={contacts[1].phone}
+        email={contacts[1].email}
+      />
+
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        mobile={contacts[2].phone}
+        email={contacts[2].email}
+      />
+
+    </div>
+  );
+}
+
+export default App;
